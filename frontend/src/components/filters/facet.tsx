49813faa14c3b4b5d0f9d079d2058572fad7362c
Joshua <joshua.brigati@gmail.com>
2023-12-08 10:16:08 -0600

    filters WIP

---
+++ frontend/src/components/filters/facet.tsx
@@ -0,0 +1,148 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+
+import { useEffect, useState } from 'react';
+import styles from './facet.module.scss';
+import { planetOptions, inhabitantOptions, objectOptions } from './options';
+import { GalleryFiltersProps } from 'pages/nft/NFTs';
+
+export const Facet = ({
+  galleryFilters,
+  setGalleryFilters,
+}: {
+  galleryFilters: GalleryFiltersProps
+  setGalleryFilters: ({
+    planetNumber,
+    planetNames,
+    planetInhabitants,
+    nftObjects
+  }: GalleryFiltersProps) => void
+}) => {
+  const [selectedPlanets, setSelectedPlanets] = useState<string[]>([]);
+  const [selectedInhabitants, setSelectedInhabitants] = useState<string[]>([]);
+  const [selectedObjects, setSelectedObjects] = useState<string[]>([]);
+
+  useEffect(() => {
+    setGalleryFilters({
+      ...galleryFilters,
+      planetNames: selectedPlanets,
+      planetInhabitants: selectedInhabitants,
+      nftObjects: selectedObjects,
+    });
+  }, [selectedPlanets, selectedInhabitants, selectedObjects]);
+
+  const clearFilters = () => {
+    setGalleryFilters({
+      planetNumber: null,
+      planetNames: [],
+      planetInhabitants: [],
+      nftObjects: [],
+    });
+    setSelectedPlanets([]);
+    setSelectedInhabitants([]);
+    setSelectedObjects([]);
+  };
+
+  const handlePlanetClick = (planetName: string) => {
+    if (selectedPlanets?.includes(planetName)) {
+      setSelectedPlanets(selectedPlanets.filter(name => name !== planetName));
+    } else {
+      setSelectedPlanets(selectedPlanets ? [...selectedPlanets, planetName] : [planetName]);
+    }
+  }
+
+  const handleInhabitantClick = (inhabitant: string) => {
+    if (selectedInhabitants?.includes(inhabitant)) {
+      setSelectedInhabitants(selectedInhabitants.filter(name => name !== inhabitant));
+    } else {
+      setSelectedInhabitants(selectedInhabitants ? [...selectedInhabitants, inhabitant] : [inhabitant]);
+    }
+  }
+
+  const handleObjectClick = (object: string) => {
+    if (selectedObjects?.includes(object)) {
+      setSelectedObjects(selectedObjects.filter(name => name !== object));
+    } else {
+      setSelectedObjects(selectedObjects ? [...selectedObjects, object] : [object]);
+    }
+  }
+
+  return (
+    <div className={styles.facet}>
+      <div className={styles.facet__header}>
+        <div className={styles.facet__title}>
+          <h3>Gallery Filters</h3>
+        </div>
+        <div className={styles.facet__clear}>
+          <button className={styles.clear__button} onClick={clearFilters}>Clear</button>
+        </div>
+      </div>
+      <div className={styles.facet__body}>
+        <div className={styles.filter__section}>
+          <div className={styles.filter__section__header}>
+            <h4>Planet Type</h4>
+          </div>
+          <div className={styles.filter__section__body}>
+            {planetOptions.map(planet => (
+              <div className={styles.facet__item}>
+                <div className={styles.facet__item__checkbox}>
+                  <input
+                    type="checkbox"
+                    onClick={() => handlePlanetClick(planet)}
+                    checked={selectedPlanets?.includes(planet)}
+                  />
+                </div>
+                <div className={styles.facet__item__name}>
+                  <span>{planet}</span>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className={styles.filter__section}>
+          <div className={styles.filter__section__header}>
+            <h4>Inhabitants</h4>
+          </div>
+          <div className={styles.filter__section__body}>
+            {inhabitantOptions.map(inhabitant => (
+              <div className={styles.facet__item}>
+                <div className={styles.facet__item__checkbox}>
+                  <input
+                    type="checkbox"
+                    onClick={() => handleInhabitantClick(inhabitant)}
+                    checked={selectedInhabitants?.includes(inhabitant)}
+                  />
+                </div>
+                <div className={styles.facet__item__name}>
+                  <span>{inhabitant}</span>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className={styles.filter__section}>
+          <div className={styles.filter__section__header}>
+            <h4>Objects</h4>
+          </div>
+          <div className={styles.filter__section__body}>
+            {objectOptions.map(nftObject => (
+              <div className={styles.facet__item}>
+                <div className={styles.facet__item__checkbox}>
+                  <input
+                    type="checkbox"
+                    onClick={() => handleObjectClick(nftObject)}
+                    checked={selectedObjects?.includes(nftObject)}
+                  />
+                </div>
+                <div className={styles.facet__item__name}>
+                  <span>{nftObject}</span>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- frontend/src/pages/nft/NFTs.tsx
+++ frontend/src/pages/nft/NFTs.tsx
@@ -1,49 +1,79 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
 import NFTItem from 'components/nft/NFTItem';
 import styles from './NFTs.module.scss';
 import { mockNFTs, signedInUserData } from 'fakeData/mockNFTs';
+import { Facet } from 'components/filters/facet';
+import { filterNFTs } from 'components/filters/helpers';
 
 const cx = classNames.bind(styles);
 
+export interface GalleryFiltersProps {
+  planetNumber: number | null
+  planetNames: string[]
+  planetInhabitants: string[]
+  nftObjects: string[]
+}
+
 export const NFTsPage = () => {
   const [isMyNFTSelected, setIsMyNFTSelected] = useState(false);
+  const [galleryFilters, setGalleryFilters] = useState<GalleryFiltersProps>({
+    planetNumber: null,
+    planetNames: [],
+    planetInhabitants: [],
+    nftObjects: [],
+  });
+
+  const [displayedNFTs, setDisplayedNFTs] = useState(mockNFTs);
+
+  useEffect(() => {
+    if (!galleryFilters.planetNames.length && !galleryFilters.planetInhabitants.length && !galleryFilters.nftObjects.length) {
+      setDisplayedNFTs(mockNFTs);
+      return;
+    }
+
+    const filtered = filterNFTs(mockNFTs, galleryFilters);
+    setDisplayedNFTs(filtered);
+  }, [galleryFilters]);
 
   return (
     <main className={styles.main}>
-      <div className={styles.buttons}>
-        <button
-          className={cx(styles.button, { [styles.button__selected]: !isMyNFTSelected })}
-          onClick={() => setIsMyNFTSelected(false)}
-        >
-          All NFTs
-        </button>
-        <button
-          className={cx(styles.button, { [styles.button__selected]: isMyNFTSelected })}
-          onClick={() => setIsMyNFTSelected(true)}
-        >
-          My NFTs
-          <span className={styles.button__count}>{signedInUserData.nftIDs.length}</span>
-        </button>
-      </div>
-      {!isMyNFTSelected ? (
-        <div className={styles.grid}>
-          {mockNFTs.map(nft => (
-            <NFTItem key={nft.id} id={nft.id} imageUrl={nft.image} title={nft.id.toString()} />
-          ))}
+      <Facet setGalleryFilters={setGalleryFilters} galleryFilters={galleryFilters} />
+      <section className={styles.main__content}>
+        <div className={styles.buttons}>
+          <button
+            className={cx(styles.button, { [styles.button__selected]: !isMyNFTSelected })}
+            onClick={() => setIsMyNFTSelected(false)}
+          >
+            All NFTs
+          </button>
+          <button
+            className={cx(styles.button, { [styles.button__selected]: isMyNFTSelected })}
+            onClick={() => setIsMyNFTSelected(true)}
+          >
+            My NFTs
+            <span className={styles.button__count}>{signedInUserData.nftIDs.length}</span>
+          </button>
         </div>
-      ) : (
-        <div className={styles.grid}>
-          {signedInUserData.nftIDs.map(nft => (
-            <NFTItem
-              key={mockNFTs[nft].id}
-              id={mockNFTs[nft].id}
-              imageUrl={mockNFTs[nft].image}
-              title={mockNFTs[nft].id.toString()}
-            />
-          ))}
-        </div>
-      )}
+        {!isMyNFTSelected ? (
+          <div className={styles.grid}>
+            {displayedNFTs.map(nft => (
+              <NFTItem key={nft.id} id={nft.id} imageUrl={nft.image} title={nft.id.toString()} />
+            ))}
+          </div>
+        ) : (
+          <div className={styles.grid}>
+            {signedInUserData.nftIDs.map(nft => (
+              <NFTItem
+                key={mockNFTs[nft].id}
+                id={mockNFTs[nft].id}
+                imageUrl={mockNFTs[nft].image}
+                title={mockNFTs[nft].id.toString()}
+              />
+            ))}
+          </div>
+        )}
+      </section>
     </main>
   );
 };
