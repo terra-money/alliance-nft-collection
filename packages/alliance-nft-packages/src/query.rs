f121e791887a1fedc185c0e94020831999d8ee7d
Philipp mager <phmager@gmail.com>
2024-03-25 10:12:45 +0100

    add rewards query

--- packages/alliance-nft-packages/src/query.rs
+++ packages/alliance-nft-packages/src/query.rs
@@ -8,12 +8,16 @@ use cw721::{
     NumTokensResponse, OperatorsResponse, OwnerOfResponse, TokensResponse,
 };
 use cw721_base::QueryMsg as CW721QueryMsg;
+use cw_asset::Asset;
 
 #[cw_serde]
 #[derive(QueryResponses)]
 pub enum QueryCollectionMsg {
     #[returns(ConfigRes)]
     Config {},
+
+    #[returns(RewardsResponse)]
+    Rewards { token_id: String },
 
     /// With MetaData Extension.
     /// Returns metadata about one particular token,
@@ -126,6 +130,11 @@ pub enum QueryCollectionMsg {
     // Return the minter
     #[returns(MinterResponse)]
     Minter {},
+}
+
+#[cw_serde]
+pub struct RewardsResponse {
+    pub rewards: Vec<Asset>,
 }
 
 #[cw_serde]
